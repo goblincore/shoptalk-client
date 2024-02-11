cc6ea768bb87df911d5f9b80f1e4b41ee6c05742
goblincore <withallmyhrt@gmail.com>
2024-02-10 22:15:30 -0500

    add display of query res

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import "./App.css";
 import ProjectInfoView from "./views/ProjectInfoView";
 import CalendarView from "./views/CalendarView";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+type ResourceInfo = {
+  department: string;
+  resourceName: string;
+  description?: string;
+};
 
 function App() {
   const payloadUrl = import.meta.env.VITE_PUBLIC_PAYLOAD_URL;
+  const [resource, setResource] = useState<ResourceInfo | undefined>();
 
-  console.log('///payloadUrl', payloadUrl);
+  console.log("///payloadUrl", payloadUrl);
 
   const fetchResourceInfo = async () => {
-    const res = await fetch(`${payloadUrl}/get-resource?resource=65c804a818f492d6d0297b79`);
-    const resJson = await res.json();
-    console.log("///get resource res json", resJson);
+    const res = await fetch(
+      `${payloadUrl}/get-resource?resource=65c804a818f492d6d0297b79`
+    );
+    try {
+      const resJson = await res.json();
+      console.log("///get resource res json", resJson);
+      setResource({
+        resourceName: resJson.name,
+        department: resJson.department.name,
+        description: resJson.description,
+      });
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   useEffect(() => {
@@ -21,6 +38,11 @@ function App() {
 
   return (
     <>
+      <section className="flex flex-col items-start justify-center">
+        <h3>{resource?.resourceName}</h3>
+        <p>{resource?.department}</p>
+        <p>{resource?.description}</p>
+      </section>
       <Tabs defaultValue="time" className="w-[full]">
         <TabsList>
           <TabsTrigger value="time">Step 1: Pick Time</TabsTrigger>
