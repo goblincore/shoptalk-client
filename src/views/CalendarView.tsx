3664b77e0efb714c5cbd0f66a270b74952728bcc
goblincore <withallmyhrt@gmail.com>
2024-02-10 19:26:33 -0500

    flesh out calendar view component stub

--- src/views/CalendarView.tsx
+++ src/views/CalendarView.tsx
@@ -1,10 +1,22 @@
+import { Calendar, dayjsLocalizer } from "react-big-calendar";
+
+import dayJs from "dayjs";
+
+const localizer = dayjsLocalizer(dayJs);
+
 const CalendarView: React.FC = () => {
-    return (
-      <section>
-        <p>ProjectInfoView</p>
-      </section>
-    );
-  };
-  
-  export default CalendarView;
-  
+  return (
+    <section>
+      <p>ProjectInfoView</p>
+      <Calendar
+        localizer={localizer}
+        //   events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </section>
+  );
+};
+
+export default CalendarView;
