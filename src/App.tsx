b972816ee0a850d0d961249998cf61768333fa2f
goblincore <withallmyhrt@gmail.com>
2024-02-10 21:01:26 -0500

    Add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,30 @@ import "./App.css";
 import MachineView from "./views/MachineView";
 import ProjectInfoView from "./views/ProjectInfoView";
 import CalendarView from "./views/CalendarView";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <MachineView />
-      <ProjectInfoView />
-      <CalendarView />
+      <Tabs defaultValue="account" className="w-[full]">
+        <TabsList>
+          <TabsTrigger value="time">Step 1: Pick Time</TabsTrigger>
+          <TabsTrigger value="project">Step 2: Project Info</TabsTrigger>
+          <TabsTrigger value="payment">Step 3: Payment Information</TabsTrigger>
+        </TabsList>
+        <TabsContent value="time">
+          <CalendarView />
+        </TabsContent>
+        <TabsContent value="project">
+          <ProjectInfoView />
+        </TabsContent>
+        <TabsContent value="payment">
+          <ProjectInfoView />
+        </TabsContent>
+      </Tabs>
     </>
   );
 }
