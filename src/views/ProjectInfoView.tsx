00c60484dce23fdd77b8cb9ec38c47b082d6bb75
goblincore <withallmyhrt@gmail.com>
2024-02-10 20:09:44 -0500

    Add placeholder form components

--- src/views/ProjectInfoView.tsx
+++ src/views/ProjectInfoView.tsx
@@ -1,7 +1,10 @@
+import ProjectInfoForm from "../components/forms/ProjectInfoForm";
+
 const ProjectInfoView: React.FC = () => {
   return (
     <section>
       <p>ProjectInfoView</p>
+      <ProjectInfoForm />
     </section>
   );
 };
